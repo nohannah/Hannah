345504ed002318473b553323a0c28153dcfb890e
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-18 15:12:59 +0700

    1st

---
+++ pi_day1.py
@@ -0,0 +1,26 @@
+import time
+from math import pi
+today = input("What is today??")
+if today == "pi day":
+	print ("Yeah!!!")
+	time.sleep(1)
+elif today== "piday":
+    print("yes that right")
+else:
+    print("let think again what is today?")
+print('can you count first 3 degit of the value of py')
+pi_degit=(input('3 degit of pi'))
+if pi_degit == "3.14":
+    print('yes that is right answer')
+    time.sleep(1)
+else:
+    print('please think again what is the 3 degit value of pi')
+print('yeah you are awsome')
+time.sleep(3)
+print('so one more thing can you count 10 degit value of pi??')
+degit_10 = input('10 defit of pi')
+if degit_10=="3.141592653":
+    print('wow you are so smart')
+else:
+    print('can you try to think again what is the first 10 degit of pi value??')
+print('')
