3b679a8f158a9c7a6ac468b219d6d32e1fb58aa5
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-29 17:53:02 +0700

    4th

--- Lesson1.py
+++ Lesson1.py
@@ -1,5 +1,6 @@
 from math import pi
 from re import T
+from subprocess import DETACHED_PROCESS
 #Ex1
 print("what do you call a bear with no teeth?")
 print("A gummy bear")
@@ -16,3 +17,8 @@ d=14*1.6
 t=(45/60)+(30/3600)
 v=d/t 
 print("The average speed is :",v)
+#ex5
+#calculate in a day 
+birth=3600/6 
+death=15 
+immigrate=45 
