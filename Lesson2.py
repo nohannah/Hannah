b7dd485aacb1aa2745133860270a53cbd8da98be
nohannah <67176677+nohannah@users.noreply.github.com>
2022-08-19 17:18:48 +0700

    19

---
+++ Lesson2.py
@@ -0,0 +1,15 @@
+# str = "Hello"
+# print(str[::-1]) 
+# n=0
+# f=3.0
+# s="Welcome"
+# print("Type of n is",type(n))
+# print("Type of n is",type(f))
+# print("Type of n is",type(s))
+#capital and smaller
+s="Welcome"
+s1=s.lower()
+print(s1)
+s2=s.upper()
+print(s2)
+
