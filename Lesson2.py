b9ed3f1499fab52ed72384a720f861b4a5a97df0
nohannah <67176677+nohannah@users.noreply.github.com>
2022-08-25 17:06:21 +0700

    25/08

--- Lesson2.py
+++ Lesson2.py
@@ -7,9 +7,25 @@
 # print("Type of n is",type(f))
 # print("Type of n is",type(s))
 #capital and smaller
-s="Welcome"
-s1=s.lower()
-print(s1)
-s2=s.upper()
-print(s2)
+# s="Welcome"
+# s1=s.lower()
+# print(s1)
+# s2=s.upper()
+# print(s2)
+##convert interger to string
+# num = 10
+  
+# # check  and print type of num variable
+# print("Type of variable before convertion : ", type(num)) 
+  
+# # convert the num into string
+# converted_num = str(num)
+  
+# # check  and print type converted_num variable
+# print("Type After convertion : ",type(converted_num))
+# Address = "House #12\nStreet 292\nOld town"
+# print(Address)
+#Reverse string from back to front
+str = "Hello"
+print(str[::-1])
 
