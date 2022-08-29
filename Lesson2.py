68983da77622020850d1124457b810100f7c34f9
nohannah <67176677+nohannah@users.noreply.github.com>
2022-08-29 16:18:08 +0700

    29

--- Lesson2.py
+++ Lesson2.py
@@ -38,10 +38,7 @@
 # print(sum)
 # s='2'+'3'
 # print(s)
-from Lesson1 import H
-
-
 first_name=str(input("Enter your first name with smaller letter:"))
 last_name=str(input("Enter the last name with smaller letter:"))
-P=str(first_name.upper + last_name.upper)
-print(P)
+X = first_name + last_name
+print(X.upper)
