a9de6d2a9f295d8df021d0d1b93e8240e1091493
nohannah <67176677+nohannah@users.noreply.github.com>
2022-08-29 16:28:33 +0700

    29

--- Lesson2.py
+++ Lesson2.py
@@ -30,7 +30,7 @@
 # print(str[::-1])
 # from math import pi
 # R=int(input("Radius number:"))
-# Deapth=int(input("Deapth number: "))
+# Deapth=int(input("Deapth number: s"))
 # C= pi*pow(R,2)
 # Volume= C* Deapth 
 # print(format(Volume,".2f"))
@@ -40,5 +40,4 @@
 # print(s)
 first_name=str(input("Enter your first name with smaller letter:"))
 last_name=str(input("Enter the last name with smaller letter:"))
-X = first_name + last_name
-print(X.upper)
+print(first_name.upper(),last_name.upper())
